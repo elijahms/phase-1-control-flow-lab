1583ed84211c7f03a62166904a8777ba4ef092c1
Elijah Silverman <elijahmsilverman@gmail.com>
2021-10-11 13:45:28 -0400

    commit tree 4c7c83a24e0294f903f476ecd498af2b1e572f0a

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  if (someNumber <= 400) {
+    return "This one is on me!"
+  } else if(someNumber> 2500) {
+    return  "No can do."
+  } else if(someNumber >2000) {
+    return "I will gladly take your thirty bucks."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  const response = destination === "NYC" ? "Ok, sounds good." : "No go.";
+  return response
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let answer;
+  switch (tip) {
+    case 'generous':
+      answer = "Thank you so much.";
+      break;
+    case 'not as generous':
+      answer = "Thank you.";
+      break;
+    default:
+      answer = 'Bye.'
+  }
+  return answer
 }
